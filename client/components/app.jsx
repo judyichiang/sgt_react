a05f14f8455a9ade8235a13dbd21ce03ad27c4cb
Judy <jiyc1119@gmail.com>
2020-03-02 17:17:48 -0800

    added header component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+
+  }
+
+  componentDidUpdate() {
+    this.getGrades();
+  }
+
+  getGrades() {
+
+  }
+
   render() {
-    return null;
+
+    function Header(props) {
+      return <h1>Student Grade Table</h1>;
+    }
+
+    return (
+      Header()
+    );
   }
 }
 
