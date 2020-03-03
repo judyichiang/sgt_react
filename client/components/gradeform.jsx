e596067dcc840632625837cf4f60d6cf52b2ef24
Judy <jiyc1119@gmail.com>
2020-03-03 14:50:13 -0800

    created new class for GradeForm

--- client/components/gradeform.jsx
+++ client/components/gradeform.jsx
@@ -1,14 +1,42 @@
 /* eslint-disable no-console */
 import React from 'react';
 
-export default function GradeForm(props) {
-  return (
-    <div className="col-md">
-      <form>
+export default class GradeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newEntry: ''
+    };
 
-      </form>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
-    </div>
-  );
+  }
+
+  handleChange(event) {
+    this.setState({
+      newEntry: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const newGrade = {
+      newEntry: this.state.newEntry
+    };
+    this.props.onSubmit(newGrade);
+    this.setState({ newEntry: '' });
+  }
+
+  render() {
+    return (
+      <div className="col-md">
+        <form>
+
+        </form>
+
+      </div>
+    );
+  }
 
 }
