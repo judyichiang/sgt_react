7c392a748708ed209a9ea3ebdb1960ac70c59490
Judy <jiyc1119@gmail.com>
2020-03-03 15:21:29 -0800

    added buttons- add & cancel

--- client/components/gradeform.jsx
+++ client/components/gradeform.jsx
@@ -41,26 +41,26 @@ export default class GradeForm extends React.Component {
             <input type="text" name="name" className="form-control" aria-label="Sizing example input"
               aria-describedby="inputGroup-sizing-sm" placeholder="Name"/>
           </div>
+
+          <div className="form-group input-group-prepend">
+            <span className="input-group-text">
+              <i className="far fa-list-alt"></i>
+            </span>
+            <input type="text" name="course" className="form-control" aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-sm" placeholder="Course"/>
+          </div>
+
+          <div className="form-group input-group-prepend">
+            <span className="input-group-text">
+              <i className="fas fa-graduation-cap"></i>
+            </span>
+
+            <input type="text" name="grade" className="form-control" aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-sm" placeholder="Student Grade"/>
+          </div>
+          <button type="reset" className="btn btn-outline-dark ml-2 float-md-right">Cancel</button>
+          <button type="submit" className="btn btn-success float-md-right">Add</button>
         </form>
-
-        <div className="form-group input-group-prepend">
-          <span className="input-group-text">
-            <i className="far fa-list-alt"></i>
-          </span>
-          <input type="text" name="course" className="form-control" aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-sm" placeholder="Course"/>
-        </div>
-
-        <div className="form-group input-group-prepend">
-          <span className="input-group-text">
-            <i className="fas fa-graduation-cap"></i>
-          </span>
-
-          <input type="text" name="grade" className="form-control" aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-sm" placeholder="Student Grade"/>
-
-        </div>
-
       </div>
     );
   }
